b9ee715333a16b2c4fe09ca2234c5c6659e23152
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-02-25 22:20:31 +0545

    fixed orders page for admin

--- apps/admin/src/components/AppSidebar.tsx
+++ apps/admin/src/components/AppSidebar.tsx
@@ -78,7 +78,7 @@ const AppSidebar = () => {
             <SidebarMenuButton asChild>
               <Link href="/">
                 <Image src="/logo.svg" alt="logo" width={20} height={20} />
-                <span>Lama Dev</span>
+                <span> Dev</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
@@ -163,7 +163,7 @@ const AppSidebar = () => {
             <SidebarMenu>
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
-                  <Link href="/users">
+                  <Link href="/orders">
                     <User />
                     See All Users
                   </Link>
