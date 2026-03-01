f1a4b3c94d32ab936084b17c6be80379a844b7ab
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-03-01 17:59:34 +0545

    trying to fetch users for the clerk

--- apps/admin/src/app/(dashboard)/users/columns.tsx
+++ apps/admin/src/app/(dashboard)/users/columns.tsx
@@ -11,18 +11,19 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
+import type { User } from "@clerk/nextjs/server";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-export type User = {
-  id: string;
-  avatar: string;
-  fullName: string;
-  email: string;
-  status: "active" | "inactive";
-};
+// export type User = {
+//   id: string;
+//   avatar: string;
+//   fullName: string;
+//   email: string;
+//   status: "active" | "inactive";
+// };
 
 export const columns: ColumnDef<User>[] = [
   {
