20b9893e00649f5907bfdd1ebb9a1ee351b4da3e
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-03-01 21:50:06 +0545

    added user endpoints

--- apps/auth-service/src/routes/user.route.ts
+++ apps/auth-service/src/routes/user.route.ts
@@ -13,4 +13,15 @@ router.get("/:id",async(req,res)=>{
     res.status(200).json(user)
 })
 
+router.post("/",async(req,res)=>{
+    type CreateParams= Parameters<typeof  clerkClient.users.createUser>[0]
+    const newUser:CreateParams=req.body
+    const user=await clerkClient.users.createUser(newUser)
+    res.status(200).json(user)
+})
+router.delete("/:id",async(req,res)=>{
+    const {id} =req.params
+    const user=await clerkClient.users.deleteUser(id)
+    res.status(200).json(user)
+})
 export default router 
