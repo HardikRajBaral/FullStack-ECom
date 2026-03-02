a1ce9f954f68e512855518750a5e59b3acf2ed3a
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-03-02 12:35:59 +0545

    fetching the data to create dynamic charts

--- apps/order-service/src/routes/order.ts
+++ apps/order-service/src/routes/order.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { shouldBeAdmin, shouldBeUser, } from "../middleware/authMiddleware";
 import { Order } from "@repo/order-db";
+import { startOfMonth, subMonths } from "date-fns";
 
 export const orderRoute= async (fastify:FastifyInstance)=>{
     fastify.get('/user-orders',{preHandler:shouldBeUser},async(request,reply)=>{
@@ -13,5 +14,48 @@ export const orderRoute= async (fastify:FastifyInstance)=>{
         return reply.send(orders);
     })
 
+    fastify.get('/order-chart',{preHandler:shouldBeAdmin},async(request,reply)=>{
+        const now= new Date()
+        const sixMonthsAgo= startOfMonth(subMonths(now,5))
+        //{ month: "June", total: 214, successful: 140 }
+        const raw= await Order.aggregate([
+            {
+                $match:{
+                    createdAt:{$gte:sixMonthsAgo,$lte:now}
+                }
+            },
+            {
+                $group:{
+                    _id:{
+                        year:{$year:"$createdAt"},
+                        month:{$month:"$createdAt"},
+                    },
+                    total:{$sum:1},
+                    successful:{
+                        $sum:{
+                            $cond:[{$eq:["status","success"]},1,0]
+                        }
+                    }
+                }
+            },
+            {
+                $project:{
+                    _id:0,
+                    year:"$_id.year",
+                    month:"$_id.month",
+                    total:1,
+                    successful:1
+                }
+            },
+            {
+                $sort:{
+                    year:1,
+                    month:1
+                }
+            }
+        ])
+
+    })
+    
 
 }
